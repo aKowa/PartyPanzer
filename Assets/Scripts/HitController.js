d0fa93b1c37a997790afaacf9e708daea3da4dd5
Andre Kowalewski <a.kowa@gmx.de>
2016-04-18 21:44:17 +0200

    CLEANED coding and project structure

---
+++ Assets/Scripts/HitController.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+public var speedPenalty = 0.5F;
+public var penaltyTime = 1F;
+
+private var mc: MovementController;
+private var canApply = true;
+
+function Awake()
+{
+	mc = this.GetComponent(MovementController);
+
+	if ( mc == null )
+	{
+		Debug.LogError("Did not get MovementController");
+	}
+}
+
+function OnTriggerEnter2D( other: Collider2D )
+{
+	if ( other.tag == "Missile" && canApply ) 
+	{
+		OffsetTime();
+		mc.moveSpeed *= speedPenalty;
+		mc.rotateSpeed *= speedPenalty;
+	}
+}
+
+function OffsetTime()
+{
+	canApply = false;
+	yield WaitForSeconds(penaltyTime);
+	mc.moveSpeed /= speedPenalty;
+	mc.rotateSpeed /= speedPenalty;
+	canApply = true;
+}
---
+++ Assets/Scripts/MovementController.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var left = KeyCode.A;
+public var right = KeyCode.D;
+
+public var moveSpeed = 1F;
+public var rotateSpeed = 1F;
+
+function Update () 
+{
+	if (Input.GetKey(left) && Input.GetKey(right))
+    {
+    	var targetPos = transform.position + transform.up * moveSpeed * Time.deltaTime;
+    	targetPos.x = Mathf.Clamp(targetPos.x,-6.32f, 6.32f);
+    	targetPos.y = Mathf.Clamp(targetPos.y,-4.7f, 4.7f);
+    	transform.position = targetPos;
+    }
+
+    if (Input.GetKey(left))
+    {
+		transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
+    }
+
+    if (Input.GetKey(right))
+    {
+		transform.Rotate(Vector3.back, -1 * rotateSpeed * Time.deltaTime);
+    }
+}
