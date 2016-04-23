593c776bcfe7001c2cac6b414c99ce1b8e031753
Andre Kowalewski <a.kowa@gmx.de>
2016-04-23 17:16:31 +0200

    ADDED shot explosion

--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class ShotController : MonoBehaviour {
 
+	public float explosionScale = 2F;
+
 	private LifeController lc;
+//	private bool isHit = false;
+	private Animator anim;
 
 	void Start()
 	{
 		lc = this.transform.parent.GetComponent<LifeController>();
+		anim = this.GetComponent<Animator>();
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -17,9 +22,18 @@ public class ShotController : MonoBehaviour {
 			MissileController mc = other.GetComponent<MissileController>();
 			if (mc.playerTag != this.transform.parent.tag)
 			{
-				Destroy(this.gameObject);
-				lc.UpdateLife(); 
+				StartCoroutine(StartExplosion());
+				lc.UpdateLife();
 			}
 		}
+	}
+
+	IEnumerator StartExplosion()
+	{
+		anim.Play("Explosion2");
+		this.GetComponent<CircleCollider2D>().enabled = false;
+		this.transform.localScale = Vector3.one * explosionScale;
+		yield return new WaitForSeconds(1f);
+		Destroy(this.gameObject);
 	}
 }
