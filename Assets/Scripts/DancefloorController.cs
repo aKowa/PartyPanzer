a49b5c720e7a3902f24a028cf4e88a8a3f549101
Andre Kowalewski <a.kowa@gmx.de>
2016-04-27 19:16:52 +0200

    ADDED dancefloorController

---
+++ Assets/Scripts/DancefloorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DancefloorController : MonoBehaviour
+{
+	public float timeTick = 1;
+	public Vector2 addOffset = new Vector2(0.5f, 0.25f);
+	private MeshRenderer rend;
+	
+	void Start()
+	{
+		this.rend = GetComponent<MeshRenderer>();
+		StartCoroutine(Wait());
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(timeTick);
+		SetOffset();
+	}
+
+	private void SetOffset()
+	{
+		Vector2 offset = rend.material.GetTextureOffset("_MainTex");
+		offset += addOffset;
+		offset.x = offset.x % 1;
+		offset.y = offset.y % 1;
+		Debug.Log(offset);
+		rend.material.SetTextureOffset("_MainTex", offset);
+		StartCoroutine(Wait());
+	}
+}
